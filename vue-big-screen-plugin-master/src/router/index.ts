d5181d3ab26566cc014c282e159d7a502b3b3558
hrnne <50048440+hrnne@users.noreply.github.com>
2023-05-21 13:33:51 +0800

    Add files via upload

--- vue-big-screen-plugin-master/src/router/index.ts
+++ vue-big-screen-plugin-master/src/router/index.ts
@@ -1,10 +1,16 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import Index2 from '../views/index/index2.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Index',
     component: () => import('../views/index/index.vue')
+  },
+  {
+    path: '/index2',
+    name: 'Index2',
+    component: Index2
   }
 ]
 
